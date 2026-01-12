635ce803ec3ecb9c682c842e392c3967dac0c720
Socialforger <socialforger@gmail.com>
2026-01-12 06:22:02 +0100

    Create buddymagiclogin.php

---
+++ buddymagiclogin.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name:       Buddy Magic Login
+ * Description:       Passwordless magic-link login and onboarding for BuddyBoss and BuddyPress.
+ * Version:           1.0.0
+ * Author:            SocialAction
+ * Text Domain:       buddymagiclogin
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+define( 'BML_VERSION', '1.0.0' );
+define( 'BML_FILE', __FILE__ );
+define( 'BML_DIR', plugin_dir_path( __FILE__ ) );
+define( 'BML_URL', plugin_dir_url( __FILE__ ) );
+
+require_once BML_DIR . 'includes/class-bml-plugin.php';
+
+add_action( 'plugins_loaded', function() {
+    \BML\Plugin::instance();
+} );
+
+register_activation_hook( __FILE__, [ '\BML\Plugin', 'activate' ] );
+register_deactivation_hook( __FILE__, [ '\BML\Plugin', 'deactivate' ] );
