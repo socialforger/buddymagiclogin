08a518f8f83a5e95e3c260e2d7332a03d097e0f8
Socialforger <socialforger@gmail.com>
2026-01-12 09:02:05 +0100

    Update access-form.php

--- templates/access-form.php
+++ templates/access-form.php
@@ -3,25 +3,36 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-use function BML\get_access_message;
-
-$message = get_access_message();
+// Messaggio dinamico (successo o errore)
+$message = apply_filters( 'bml_access_message', '' );
 ?>
-<form method="post" class="bml-access-form">
+
+<div class="bml-access-wrapper">
+
     <?php if ( ! empty( $message ) ) : ?>
-        <div class="bml-message"><?php echo esc_html( $message ); ?></div>
+        <p class="bml-message"><?php echo esc_html( $message ); ?></p>
     <?php endif; ?>
 
-    <p>
-        <label for="bml_email"><?php esc_html_e( 'Email address', 'buddymagiclogin' ); ?></label><br />
-        <input type="email" name="bml_email" id="bml_email" required />
-    </p>
+    <form method="post" class="bml-access-form">
 
-    <?php wp_nonce_field( 'bml_access', 'bml_nonce' ); ?>
+        <label for="bml_email">
+            <?php esc_html_e( 'Email address', 'buddymagiclogin' ); ?>
+        </label>
 
-    <p>
-        <button type="submit" class="button button-primary">
-            <?php esc_html_e( 'Send magic login link', 'buddymagiclogin' ); ?>
+        <input
+            type="email"
+            id="bml_email"
+            name="bml_email"
+            required
+            autocomplete="email"
+        />
+
+        <?php wp_nonce_field( 'bml_access', 'bml_nonce' ); ?>
+
+        <button type="submit" class="bml-submit">
+            <?php esc_html_e( 'Send', 'buddymagiclogin' ); ?>
         </button>
-    </p>
-</form>
+
+    </form>
+
+</div>
